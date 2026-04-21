f69e50c31f1cf2d10dc799ee9c5601347d3deaf4
Petra Mitroova <petra.mitroova@gmail.com>
2026-04-21 13:37:49 +0200

    model_service prepared

--- model_api/service/model_service.py
+++ model_api/service/model_service.py
@@ -4,4 +4,15 @@ model_router = APIRouter()
 
 @model_router.get("/models")
 def get_all_models():
+    """Vypise zoznam vsektych dostupnych modelov k predikcii """
     return {"models": []}
+
+@model_router.get("/models/{model_name")
+def get_model(model_name:str):
+    """Vypise info o konkretnom modele"""
+    return model_name
+
+@model_router.post("/predicts/{model_name}")
+def predict(model_name:str,data:dict):
+    """Predikcia so zvolenym modelom"""
+    pass
